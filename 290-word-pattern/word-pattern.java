16c53d799843af90ac1265f4b581cb4e682a10f7
Harinni31 <harinnishanmugam31@gmail.com>
2025-12-28 12:03:03 +0530

    Time: 1 ms (80.83%) | Memory: 42.7 MB (68.31%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] str = s.split(" ");
+        char[] c = pattern.toCharArray();
+        if(c.length!=str.length) return false;
+        HashMap<Character,String> map=new HashMap<>();
+        HashMap<String,Character> m=new HashMap<>();
+        for(int i=0;i<c.length;i++)
+        {
+            if(map.containsKey(c[i]))
+            {
+                 if(!map.get(c[i]).equals(str[i]))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+               if(m.containsKey(str[i]))
+               {
+                 if(!m.get(str[i]).equals(c[i]))
+                {
+                    return false;
+                }
+               }
+            }
+            map.put(c[i],str[i]);
+            m.put(str[i],c[i]);
+        }
+        return true;
+    }
+}
