f11003efaa285bda2d7bc1bb2333f9ce18a8a1ae
Harinni31 <harinnishanmugam31@gmail.com>
2025-12-29 13:33:07 +0530

    Time: 1 ms (63.92%) | Memory: 43.3 MB (28.73%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,5 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        /*
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<strs[0].length();i++)
         {
@@ -15,5 +16,22 @@ class Solution {
             sb.append(ch);
         }
           return sb.toString();
+          */
+          // StringBuilder sb=new StringBuilder();
+          if(strs.length==0) return " ";
+        for(int i=0;i<strs[0].length();i++)
+        {
+            char ch=strs[0].charAt(i);
+           
+            for(int j=1;j<strs.length;j++)
+            {
+                if(i>=strs[j].length() || strs[j].charAt(i)!=ch )
+                {
+                   return strs[0].substring(0,i);
+                }
+            }
+            //sb.append(ch);
+        }
+          return strs[0] ;
  }
 }
