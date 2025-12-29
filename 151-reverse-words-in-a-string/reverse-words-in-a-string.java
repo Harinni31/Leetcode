bc98dea1c2147cd36d0e8c82653fd77c103ce446
Harinni31 <harinnishanmugam31@gmail.com>
2025-12-29 18:15:44 +0530

    Time: 3 ms (98.72%) | Memory: 44.1 MB (84.49%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String reverseWords(String s) {
-        s=s.trim();
+     /*   s=s.trim();
         String[] str=s.split(" ");
         StringBuilder sb=new StringBuilder();
         for(int i=str.length-1;i>=0;i--)
@@ -12,6 +12,19 @@ class Solution {
             }
         }
         
+        return sb.toString().trim();
+        */
+        int i=s.length()-1;
+        StringBuilder sb=new StringBuilder();
+        while(i>=0)
+        {
+            while(i>=0 && s.charAt(i)==' ') i--;//last index before space from end j+1-->i
+            int j=i;
+            while(j>=0 && s.charAt(j)!=' ') j--;// start index
+            sb.append(s.substring(j+1,i+1));
+            sb.append(" ");
+            i=j-1;
+        }
         return sb.toString().trim();
     }
 }
