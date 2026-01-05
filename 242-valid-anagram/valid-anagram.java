1d5b6f6f92b1a408f67fceea7392348367111eea
Harinni31 <harinnishanmugam31@gmail.com>
2026-01-05 19:24:33 +0530

    Time: 2 ms (99.47%) | Memory: 44.8 MB (56.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
         int[] freq=new int[26];
         for(char c:s.toCharArray())
         {
@@ -7,11 +8,6 @@ class Solution {
         }
         for(char c:t.toCharArray())
         {
-          /*  if(freq[c-'a']==0)
-            {
-                return false;
-            }
-            */
             freq[c-'a']--;
         }
         for(int num:freq)
