69d50c1f02e1fc5b2afc32abd29804ee0e77f6c3
Harinni31 <harinnishanmugam31@gmail.com>
2026-01-11 21:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.03%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+     long res=1;
+     int r=Math.min(m-1,n-1);
+     for(int i=1;i<=r;i++)
+     {
+        res=res*(m+n-2-r+i)/i;
+     }
+     return (int)res;
+    }
+}
