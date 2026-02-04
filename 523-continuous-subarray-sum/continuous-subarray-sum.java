456d9cef5d3c3d050fc752b89380349ba919f9f3
Harinni31 <harinnishanmugam31@gmail.com>
2026-02-04 23:59:37 +0530

    Time: 26 ms (32.86%) | Memory: 140 MB (15.02%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -3,7 +3,7 @@ class Solution {
     {   HashMap<Integer,Integer> map=new HashMap<>();
     int sum=0;
     map.put(0,-1);
-        //int maxlen=0;
+
         for(int i=0;i<nums.length;i++)
         {
             sum+=nums[i];
