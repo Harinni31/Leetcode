9aa305eeb30ce527673363de7f17c18b7be56a1c
Harinni31 <harinnishanmugam31@gmail.com>
2026-07-07 00:15:31 +0530

    Time: 40 ms (26.67%) | Memory: 60.2 MB (7.14%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,9 +1,31 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return atmost(nums,k)-atmost(nums,k-1);
+      //  return atmost(nums,k)-atmost(nums,k-1);
+      HashMap<Integer,Integer>map=new HashMap<>();
+        int sum=0;
+      int count=0;
+      map.put(0,1);
+      for(int num:nums)
+      {
+        if(num%2!=0)
+        {
+            sum+=1;
+        }
+        if(sum >= k)
+        {
+            if(map.containsKey(sum-k))
+            {
+                count+=map.get(sum-k);
+            }
+            
+        }
+        map.put(sum,map.getOrDefault(sum,0)+1);
+      }
+      return count;
     }
-    private int atmost(int[] nums,int k)
-    {
+  /*  private int atmost(int[] nums,int k)
+    {  
+        
         int left=0;
         int count=0;
         int nice=0;
@@ -18,5 +40,5 @@ class Solution {
             nice+=(right-left+1);
         }
         return nice;
-    }
+    }*/
 }
