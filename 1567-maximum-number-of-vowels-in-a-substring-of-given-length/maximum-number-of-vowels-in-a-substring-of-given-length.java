c41f9325621d6d175fc35efd8a2524a879932c4a
Harinni31 <harinnishanmugam31@gmail.com>
2026-07-18 14:54:01 +0530

    Time: 12 ms (80.28%) | Memory: 46.5 MB (34.63%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,8 +1,15 @@
 class Solution {
+    private static boolean isVowel(char c)
+    {
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' )
+        {
+            return true;
+        }
+        return false;
+    }
     public int maxVowels(String s, int k) {
         int count=0;
-        int maxcount=0;
-        int left=0;
+        
         for(int i=0;i<k;i++)
         {
             if(isVowel(s.charAt(i)))
@@ -10,26 +17,60 @@ class Solution {
                 count++;
             }
         }
-        maxcount=count;
-        for(int right=k;right<s.length();right++)
+        int maxlen=count;
+        if (s.length()==k) return maxlen;
+        for(int i=k;i<s.length();i++)
         {
-            if(isVowel(s.charAt(right)))
+            if(isVowel(s.charAt(i)))
             {
                 count++;
             }
-             if(isVowel(s.charAt(right-k)))
+                   if(isVowel(s.charAt(i-k)))
             {
                 count--;
             }
-         maxcount=Math.max(maxcount,count);
+            maxlen=Math.max(maxlen,count);
         }
-        return maxcount;
-    }
-    public boolean isVowel(char c)
-    {
-        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
-        return true;
-                
-        return false;
+
+
+
+
+
+
+
+
+
+
+
+   /*     int maxvowels=0;
+        int winv=0;
+        Set<Character> set=new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        for(int i=0;i<k;i++)
+        {
+            if(set.contains(s.charAt(i)))
+            {
+                winv++;
+            }
+        }
+        maxvowels=winv;
+        for(int i=k;i<s.length();i++)
+        {
+            if(set.contains(s.charAt(i)))
+            {
+                winv++;
+            }
+            if(set.contains(s.charAt(i-k)))
+            {
+                winv--;
+            }
+            maxvowels=Math.max(maxvowels,winv);
+        }
+        return maxvowels;*/
+    return maxlen;
     }
 }
