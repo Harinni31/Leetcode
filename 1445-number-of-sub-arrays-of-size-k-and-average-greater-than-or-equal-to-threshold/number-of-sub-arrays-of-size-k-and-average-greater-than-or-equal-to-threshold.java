a3f4b45f8113f6d16bb3629fbdc328c055e7b382
Harinni31 <harinnishanmugam31@gmail.com>
2026-07-18 20:14:35 +0530

    Time: 3 ms (86.11%) | Memory: 72 MB (7.60%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,25 +1,28 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-            int count=0;
-            double sum=0;
-            for(int i=0;i<k;i++)
-            {
-                sum+=arr[i];
-            }
-            double avg=sum/k;
-            if(avg>=threshold) count++;
-            for(int i=k;i<arr.length;i++)
-            {
-              //  if(avg>=threshold) count--;
-                sum+=arr[i]-arr[i-k];
-                avg=sum/k;
-                if(avg>=threshold) count++;
-                
-            }
-
-            return count;
+        int count=0;
+        int sum=0;
+        for(int i=0;i<k;i++)
+        {
+            sum+=arr[i];
+        }
+     
+        if(sum>=threshold * k)
+        {
+            count++;
+        }
+        //maxcount=arrcount; avoids division more precision
+        for(int i=k;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            sum-=arr[i-k];
+            //sum/k=threshold;
+           if(sum>=threshold * k)
+        {
+            count++;
+            
+        }
+        }
+        return count;
     }
 }
-
-
-
