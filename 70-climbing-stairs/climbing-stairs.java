c6962a3d5e6c24f601c75070740ad8aa79bd266f
Harinni31 <harinnishanmugam31@gmail.com>
2026-08-01 19:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,7 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
         if(n==1) return 1;
-        int[] dp=new int[n+1];
+       int prev2=1;
+       int prev1=2;
+       for(int i=3;i<=n;i++)
+        {
+           int current=prev1+prev2;
+           prev2=prev1;
+           prev1=current;
+        }
+        return prev1;
+    }
+
+}
+/*
+ int[] dp=new int[n+1];
         dp[1]=1;
         dp[2]=2;
         for(int i=3;i<=n;i++)
@@ -9,5 +22,4 @@ class Solution {
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-    }
-}
+        */
