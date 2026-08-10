9b533a6b72debaeb37b52c547b4ed8a4c7b4e196
Harinni31 <harinnishanmugam31@gmail.com>
2026-08-10 06:35:23 +0530

    Time: 2 ms (70.90%) | Memory: 55.9 MB (18.39%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,37 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
+   int vote=1;
+   int maj=nums[0];
+   for(int i=1;i<nums.length;i++)
+   {
+        if(nums[i]==maj)
+        {
+            vote++;
+        }
+        else
+        {
+            vote--;
+        }
+        if(vote==0)
+        {
+            maj=nums[i];
+            vote=1;
+        }
+
+   }
+   int count=0;
+   for(int i=0;i<nums.length;i++)
+   {
+    if(nums[i]==maj)
+    {
+        count++;
+    }
+   }
+   return count>nums.length/2?maj:-1;
+    }
+}
+/*
+     HashMap<Integer,Integer>map=new HashMap<>();
         for(int num:nums)
         {
             map.put(num,map.getOrDefault(num,0)+1);
@@ -13,5 +44,4 @@ class Solution {
             }
         }
         return -1;
-    }
-}
+        */
